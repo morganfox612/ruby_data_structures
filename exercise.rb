1a41bc0a6eaa5297bc0f2b0594586d32b13be355
Morgan Fox <morganfox61297@gmail.com>
2019-06-17 16:29:14 -0400

    exercise

---
+++ exercise.rb
@@ -0,0 +1,12 @@
+
+Organization = [
+  [Shareholders: ["Morgan", "Matthew", "Maria"]],
+  [Executives: ["Joshua", "John", "Joe"]],
+  [Presidents: ["Ellen", "Elizabeth", "Elijah"]],
+  [Managers: ["Willow", "William", "Wayne"]],
+  [Employees: ["Sandra", "Sonic", "Sage"]],
+  [Samus_Mains: ["Peach", "Mario", "Luigi"]]
+]
+
+#Organization[0] > Shareholders: Morgan, Matthew, Maria
+#Organization[3] > Managers: Willow, william, Wayne
